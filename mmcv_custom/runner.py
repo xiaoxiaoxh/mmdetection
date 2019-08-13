c19ee4647da9d3a6fd45b33e72af7488be92fd85
v-xuehan <xiaoxiaoxh@sjtu.edu.cn>
2019-08-13 16:21:06 +0800

    add lr and epoch in log vars

--- mmcv_custom/runner.py
+++ mmcv_custom/runner.py
@@ -38,6 +38,31 @@ class Runner(mmcv.runner.Runner):
                 'but got {}'.format(type(optimizer)))
         return optimizer
 
+    def train(self, data_loader, **kwargs):
+        self.model.train()
+        self.mode = 'train'
+        self.data_loader = data_loader
+        self._max_iters = self._max_epochs * len(data_loader)
+        self.call_hook('before_train_epoch')
+        for i, data_batch in enumerate(data_loader):
+            self._inner_iter = i
+            self.call_hook('before_train_iter')
+            outputs = self.batch_processor(
+                self.model, data_batch, train_mode=True, **kwargs)
+            if not isinstance(outputs, dict):
+                raise TypeError('batch_processor() must return a dict')
+            outputs['log_vars']['lr'] = self.current_lr()[0]  # add lr in log variables
+            outputs['log_vars']['epoch'] = self.epoch  # add epoch in log variables
+            if 'log_vars' in outputs:
+                self.log_buffer.update(outputs['log_vars'],
+                                       outputs['num_samples'])
+            self.outputs = outputs
+            self.call_hook('after_train_iter')
+            self._iter += 1
+
+        self.call_hook('after_train_epoch')
+        self._epoch += 1
+
     def save_checkpoint(self,
                         out_dir,
                         filename_tmpl='epoch_{}.pth',
