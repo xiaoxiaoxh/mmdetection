cea39863c5e44bc01d9e02bdea9e7ff42c2b998a
v-xuehan <xiaoxiaoxh@sjtu.edu.cn>
2019-08-19 20:52:39 +0800

    change some configs

--- configs/lvis/fcos_r50_caffe_fpn_gn_1x_4gpu.py
+++ configs/lvis/fcos_r50_caffe_fpn_gn_1x_4gpu.py
@@ -111,7 +111,7 @@ optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
     policy='step',
-    warmup='constant',
+    warmup='linear',
     warmup_iters=500,
     warmup_ratio=1.0 / 3,
     step=[8, 11])
