47eb69f7052e2ccc3b2501010c8d114fe7401486
v-xuehan <xiaoxiaoxh@sjtu.edu.cn>
2019-09-02 11:30:00 +0800

    change configs

--- configs/lvis/retinanet_r50_fpn_1x_ghm.py
+++ configs/lvis/retinanet_r50_fpn_1x_ghm.py
@@ -66,8 +66,8 @@ data_root = 'data/LVIS/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 data = dict(
-    imgs_per_gpu=2,
-    workers_per_gpu=2,
+    imgs_per_gpu=1,
+    workers_per_gpu=1,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'lvis_v0.5_train.json',
