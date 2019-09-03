06d485917e0f009c3ce0e89fc57ed33f22cf83c8
v-xuehan <xiaoxiaoxh@sjtu.edu.cn>
2019-09-03 16:55:41 +0800

    add autoscale-bs

--- tools/train.py
+++ tools/train.py
@@ -46,6 +46,10 @@ def parse_args():
         '--autoscale-lr',
         action='store_true',
         help='automatically scale lr with the number of gpus and imgs per gpu')
+    parser.add_argument(
+        '--autoscale-bs',
+        action='store_true',
+        help='automatically scale imgs per gpu')
     args = parser.parse_args()
     if 'LOCAL_RANK' not in os.environ:
         os.environ['LOCAL_RANK'] = str(args.local_rank)
@@ -77,9 +81,10 @@ def main():
     # update gpu num
     if dist.is_initialized():
         cfg.gpus = dist.get_world_size()
-    if args.autoscale_lr:
+    if args.autoscale_bs:
         cfg.data.imgs_per_gpu = int(cfg.data.imgs_per_gpu / (cfg.gpus / 4))
         cfg.data.workers_per_gpu = int(cfg.data.workers_per_gpu / (cfg.gpus / 4))
+    if args.autoscale_lr:
         # apply the linear scaling rule (https://arxiv.org/abs/1706.02677)
         cfg.optimizer['lr'] = cfg.optimizer['lr'] * cfg.gpus / 8 * cfg.data.imgs_per_gpu / 2
 
