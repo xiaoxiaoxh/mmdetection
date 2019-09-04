707ce1de0002e64472232438172f6c99effe6938
v-xuehan <xiaoxiaoxh@sjtu.edu.cn>
2019-09-04 21:15:22 +0800

    change configs

--- configs/lvis/mask_rcnn_r50_fpn_1x_anno_0x5_norm.py
+++ configs/lvis/mask_rcnn_r50_fpn_1x_anno_0x5_norm.py
@@ -100,7 +100,7 @@ train_cfg = dict(
             neg_pos_ub=-1,
             add_gt_as_proposals=True),
         use_anno_info=True,  # only for LVIS
-        ignore_cls_weight=0.1,  # only for LVIS
+        ignore_cls_weight=0.5,  # only for LVIS
         ignore_cls_normalize=True,  # normalize weights of classes to C
         mask_size=28,
         pos_weight=-1,
@@ -185,7 +185,7 @@ evaluation = dict(interval=3)
 total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/mask_rcnn_r50_fpn_1x_anno_0x1_norm'
+work_dir = './work_dirs/mask_rcnn_r50_fpn_1x_anno_0x5_norm'
 load_from = None
 resume_from = None
 auto_resume = True
