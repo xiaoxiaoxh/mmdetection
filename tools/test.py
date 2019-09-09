cc263b357c10e08bb766337c037e6f395c060e9e
v-xuehan <xiaoxiaoxh@sjtu.edu.cn>
2019-09-09 13:28:55 +0800

    change configs

--- tools/test.py
+++ tools/test.py
@@ -167,9 +167,9 @@ def main():
         torch.backends.cudnn.benchmark = True
     cfg.model.pretrained = None
     cfg.data.test.test_mode = True
-    if cfg.dataset_type == 'LvisDataSet':
-        cfg.test_cfg.rcnn.max_per_img = 300
-        cfg.test_cfg.rcnn.score_thr = 0.01
+    # if cfg.dataset_type == 'LvisDataSet':
+    #     cfg.test_cfg.rcnn.max_per_img = 300
+    #     cfg.test_cfg.rcnn.score_thr = 0.01
 
     # init distributed env first, since logger depends on the dist info.
     if args.launcher == 'none':
