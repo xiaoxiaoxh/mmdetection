ffa63c50f0be4959f462074828b9c69bbb5d4ac2
v-xuehan <xiaoxiaoxh@sjtu.edu.cn>
2019-09-16 10:15:36 +0800

    fix some bug in runner

--- mmcv_custom/runner.py
+++ mmcv_custom/runner.py
@@ -231,7 +231,7 @@ class Runner(mmcv.runner.Runner):
                 else:
                     for param in module.parameters():
                         param.requires_grad = True
-            if not resume_optimizer:
+            if stage_epoch == 0:
                 torch.nn.init.normal_(model.bbox_head.fc_cls.weight, 0, 0.01)
                 torch.nn.init.constant_(model.bbox_head.fc_cls.bias, 0)
 
