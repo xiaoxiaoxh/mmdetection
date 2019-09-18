2fb0cca61b07a7ccfbe30ac03c5d59a634171766
v-xuehan <xiaoxiaoxh@sjtu.edu.cn>
2019-09-18 14:16:38 +0800

    change some configs

--- configs/lvis/mask_rcnn_r50_fpn_1x_base_debug.py
+++ configs/lvis/mask_rcnn_r50_fpn_1x_base_debug.py
@@ -164,7 +164,7 @@ optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 lr_config = dict(
     policy='step',
     warmup='linear',
-    warmup_iters=500,
+    warmup_iters=1000,
     warmup_ratio=0.01,
     step=[8, 11])
 checkpoint_config = dict(interval=1)
